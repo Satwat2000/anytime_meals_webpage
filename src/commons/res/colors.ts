b1ede722125568c8682c03b9759c17157f39abd6
Satwat2000 <satwatkr@gmail.com>
2021-08-10 01:35:45 +0530

    chore: add color reference file

---
+++ src/commons/res/colors.ts
@@ -0,0 +1,15 @@
+export default {
+  deepRed: 'rgba(117, 52, 34, 100)', //'#753422'
+  lightBrown: '#D79771',
+  lightPitch: '#FFEBC9',
+
+  blue: 'rgba(73, 119, 239, 100)',
+  tintBlue: 'rgba(73, 119, 239, 25)',
+  footer: '#67697B',
+  headerbg: '#0F0E18',
+  headerRed: 'rgba(222, 114, 114, 100)',
+  lightheaderRed: 'rgba(222, 114, 114, 5)',
+
+  dirtyWhite: '#F3EEEE',
+  lightWhite: '#FBFBFF',
+}
