6a0329f561c8f963685300686aadc7b3b5e58dc4
Satwat2000 <satwatkr@gmail.com>
2021-08-18 10:26:49 +0530

    feat: add route component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import NavBar from './components/NavBar'
-
+import MyRoutes from './routes/route'
 const App: React.FC = () => {
   return (
     <>
       <NavBar />
+      <MyRoutes />
     </>
   )
 }
