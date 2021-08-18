d444d5aa6f969209e9e2dc00905ddcf5566d9481
Shefali35 <64395804+Shefali35@users.noreply.github.com>
2021-08-18 14:31:14 +0530

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,28 @@
 import React from 'react'
-
-const Header: React.FC = () => {
-  return <></>
+import './styles.modules.css'
+const Footer: React.FC = () => {
+   return (
+    <div className={styles.foot}>
+      <span id={styles.spa}>Join us on</span>
+      <span id={styles.spa1}>Contact Us</span>
+      <ul className={styles.change}>
+        <span id={styles.spa2}>About Us</span>
+        <li>
+          <img src={process.env.PUBLIC_URL + '/images/face.png'} alt='facebook' />
+          Facebook
+        </li>
+        <br />
+        <li>
+          <img src={process.env.PUBLIC_URL + '/images/instagram.png'} alt='insta' />
+          Instagram
+        </li>
+        <br />
+        <li>
+          <img src={process.env.PUBLIC_URL + '/images/linkedin.png'} alt='lin' />
+          LinkedIn
+        </li>
+      </ul>
+    </div>
+  )
 }
-export default Header
+export default Footer
