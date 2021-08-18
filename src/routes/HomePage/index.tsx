7f99f2c1ac5fd363f71e7b3b24c8cbd8c1b7154c
Satwat2000 <satwatkr@gmail.com>
2021-08-18 10:39:22 +0530

    feat: create 'root'(home) route page

---
+++ src/routes/HomePage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage: React.FC = () => {
+  return (
+    <>
+      <div>ok ji Home Pe swagat</div>
+      <div>Hello ji</div>
+    </>
+  )
+}
+
+export default HomePage
