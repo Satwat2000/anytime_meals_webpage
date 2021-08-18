ea3e3040314b391f376db3dd5cab6491e218b334
Satwat2000 <satwatkr@gmail.com>
2021-08-18 10:40:22 +0530

    feat: create 'review' route page

---
+++ src/routes/ReviewPage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ReviewPage: React.FC = () => {
+  return (
+    <>
+      <div>ok ji Review Pe swagat</div>
+      <div>Hello ji</div>
+    </>
+  )
+}
+
+export default ReviewPage
