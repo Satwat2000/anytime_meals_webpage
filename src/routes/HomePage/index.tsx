4d346950ce21b533433fc3f5c86b53033d9e21f7
Satwat2000 <satwatkr@gmail.com>
2021-08-31 22:18:20 +0530

    refactor: home page

--- src/routes/HomePage/index.tsx
+++ src/routes/HomePage/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import ItemDisplayTag from '../../commons/ItemDisplayTag'
 // import BasicCard from '../../components/BasicCard'
-import HomeBanner from '../../components/HomeBanner'
 const HomePage: React.FC = () => {
   const Dec =
     'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s'
   return (
     <>
-      <HomeBanner />
       {/* <BasicCard imgPath='' category='mai pagalhu' /> */}
       <ItemDisplayTag
         imgName='Test'
