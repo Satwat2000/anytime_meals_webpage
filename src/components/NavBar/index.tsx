06662ed74a2d64807369d567920446f7e9702f7d
Satwat2000 <satwatkr@gmail.com>
2021-09-01 12:49:49 +0530

    feat: add navbar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,22 +1,31 @@
-import React, { useState } from 'react'
+import React from 'react'
 import NavLink from '../../commons/NavLink'
 import styles from './styles.module.css'
 import DropDown from '../../commons/DropDown'
-import { useHistory } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
 
 const NavBar: React.FC = () => {
   const history = useHistory()
+  const location = useLocation()
 
   const handleRoute = () => {
     history.push('/')
   }
 
-  const [isNavOpaque, setisNavOpaque] = useState(false)
+  const isNavOpaque = () => {
+    const isHome = location.pathname === '/'
+    if (isHome) {
+      document.body.style.margin = '0 0 0 0'
+    } else {
+      document.body.style.margin = '60px 0 0 0'
+    }
+    return !isHome
+  }
 
   return (
     <div className={styles.navBar}>
-      <div className={isNavOpaque ? `${styles.nav} ${styles.navOpaque}` : styles.nav}>
-        <div className={isNavOpaque ? styles.transperantNav : `${styles.transperantNav} ${styles.navTransparent}`} />
+      <div className={isNavOpaque() ? styles.transperantNav : `${styles.transperantNav} ${styles.navTransparent}`} />
+      <div className={isNavOpaque() ? `${styles.nav} ${styles.navOpaque}` : styles.nav}>
         <img src='/images/icons/Wlogo.png' alt='' className={styles.Wlogo} onClick={handleRoute} />
         <div className={styles.nav_options}>
           <NavLink to='/product' linkType='NavLink'>
