b700122a623f90b6e0e5cc39c822a8a825262ed5
Satwat2000 <satwatkr@gmail.com>
2021-09-01 18:49:06 +0530

    refactor: reface func with hooks

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavLink from '../../commons/NavLink'
 import styles from './styles.module.css'
 import DropDown from '../../commons/DropDown'
@@ -6,26 +6,27 @@ import { useHistory, useLocation } from 'react-router-dom'
 
 const NavBar: React.FC = () => {
   const history = useHistory()
-  const location = useLocation()
+  const path = useLocation().pathname
+  const [isHome, setisHome] = useState(true)
 
   const handleRoute = () => {
     history.push('/')
   }
 
-  const isNavOpaque = () => {
-    const isHome = location.pathname === '/'
-    if (isHome) {
+  useEffect(() => {
+    if (path === '/') {
+      setisHome(true)
       document.body.style.margin = '0 0 0 0'
     } else {
+      setisHome(false)
       document.body.style.margin = '60px 0 0 0'
     }
-    return !isHome
-  }
+  }, [path])
 
   return (
     <div className={styles.navBar}>
-      <div className={isNavOpaque() ? styles.transperantNav : `${styles.transperantNav} ${styles.navTransparent}`} />
-      <div className={isNavOpaque() ? `${styles.nav} ${styles.navOpaque}` : styles.nav}>
+      <div className={isHome ? `${styles.transperantNav} ${styles.navTransparent}` : styles.transperantNav} />
+      <div className={isHome ? styles.nav : `${styles.nav} ${styles.navOpaque}`}>
         <img src='/images/icons/Wlogo.png' alt='' className={styles.Wlogo} onClick={handleRoute} />
         <div className={styles.nav_options}>
           <NavLink to='/product' linkType='NavLink'>
